4d340e3074702e01cc06e63697cd4fcc09930bf9
Tibi <Tibowl@users.noreply.github.com>
2021-04-04 00:58:56 +0200

    Update credits

--- src/commands/misc/credits.ts
+++ src/commands/misc/credits.ts
@@ -15,8 +15,9 @@ export default class Credits extends Command {
     }
 
     async run(message: Message): Promise<Message | Message[]> {
-        return message.channel.send(`This is an open-source bot created by @${(await client.users.fetch("127393188729192448")).tag}. The source-code is available on GitHub: <https://github.com/Tibowl/HuTao>.
+        const me = (await client.users.fetch("127393188729192448")).tag
+        return message.channel.send(`This is an open-source bot created by @${me}. The source-code is available on GitHub: <https://github.com/Tibowl/HuTao>.
 
-Data is compiled from a vareity of sources by `)
+Data is compiled from a vareity of sources by ${me} as well. Feel free to contact me in case there's an error/need support via <https://discord.gg/BM3Srp8j8G>`)
     }
 }
