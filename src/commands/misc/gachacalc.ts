999477aef7a4df695d313576397229c6f7e8cef9
Tibi <Tibowl@users.noreply.github.com>
2021-04-26 02:29:01 +0200

    Add some more optimalisations

--- src/commands/misc/gachacalc.ts
+++ src/commands/misc/gachacalc.ts
@@ -11,12 +11,18 @@ const Logger = log4js.getLogger("GachaCalc")
 const gachas: Record<string, Banner> = {
     char: {
         banner: 0.5,
+        guaranteed: 1,
+        maxConst: 6,
+        maxPity: 90,
         rate: (pity) => pity < 74 ? 0.6 : [6.8, 12.7, 18.8, 24.9, 30.7, 37.1, 42.3, 48.6, 55, 60.2, 62.3, 70, 75, 80, 90, 95, 100][pity - 74] ?? 100
     }
 }
 
 type Banner = {
     banner: number
+    guaranteed: number
+    maxConst: number
+    maxPity: number
     rate: (pity: number) => number
 }
 
@@ -39,8 +45,11 @@ export default class GachaCalc extends Command {
 
 NOTE: Rates with high pity might not be accurate, not a lot of data in this rang...
             
-Example with just amount of pulls (assumes char banner, 50/50 limited, 0 pity): \`${config.prefix}gachacalc 70\``,
-            usage: "gachacalc <pulls>",
+Example with just amount of pulls (assumes char banner, 50/50 limited, 0 pity): \`${config.prefix}gachacalc 70\`
+Example with 70 pulls and 10 pity: \`${config.prefix}gachacalc 70 10\`
+Example with 70 pulls, 10 pity and guaranteed: \`${config.prefix}gachacalc 70 10 y\`
+`,
+            usage: "gachacalc <pulls> [pity] [guaranteed]",
             aliases: ["gc", "gachasim", "gcalc", "gsim", "gs"]
         })
     }
@@ -49,11 +58,11 @@ Example with just amount of pulls (assumes char banner, 50/50 limited, 0 pity): 
         if (args.length <= 0) return this.sendHelp(message)
 
         const pulls = parseInt(args[0] ?? "75")
-        if (isNaN(pulls) || pulls <= 0 || pulls > 1000)
+        if (isNaN(pulls) || pulls <= 0 || pulls > 9999)
             return message.channel.send("Invalid pulls amount, should be a number greater than 1")
 
         const pity = parseInt(args[1] ?? "0")
-        if (isNaN(pity) || pity < 0 || pity >= 90)
+        if (isNaN(pity) || pity < 0 || pity >= 90) // TODO check banner
             return message.channel.send("Invalid pity amount, should be a number between 0 (inclusive) and 90 (exclusive)")
 
         let guaranteed = false
@@ -63,6 +72,8 @@ Example with just amount of pulls (assumes char banner, 50/50 limited, 0 pity): 
             guaranteed = false
         else if (args[2])
             return message.channel.send("Invalid 50/50, should be y(es)/g(uaranteed) or n(o)/50/50")
+
+        // TODO different banner support
 
         const start = Date.now()
         const sims = this.calcSims(pity, pulls, guaranteed, "char")
@@ -80,15 +91,24 @@ ${createTable(
 \`\`\``)
     }
 
-    calcSims = memoize(this.calcSimsRegular, { max: 100 })
+    calcSims = memoize(this.calcSimsRegular, { max: 1 })
 
-    private calcSimsRegular(pity: number, pulls: number, guaranteed: boolean, banner: string): ReducedSim[] {
+    private calcSimsRegular(pity: number, pulls: number, guaranteed: boolean, bannerName: string): ReducedSim[] {
+        const banner = gachas[bannerName]
+
+        // Max pity / const
+        if (banner.guaranteed >= 1 && pulls + pity >= banner.maxPity * (banner.maxConst * 2 - (guaranteed ? 1 : 0)))
+            return [{
+                const: banner.maxConst,
+                rate: 1
+            }]
+
         return this.calcSimsInt({
             pity,
             guaranteed,
             const: -1,
             rate: 1
-        }, pulls, gachas[banner])
+        }, pulls, banner)
     }
 
     private calcSimsInt(starterSim: Sim, pulls: number, banner: Banner): ReducedSim[] {
@@ -99,17 +119,15 @@ ${createTable(
         sims.forEach((sim: Sim) => {
             if (sim.rate == 0) return
 
-            const other = reducedSims.find(s =>
-                s.const == sim.const
-            )
+            const other = reducedSims[sim.const + 1]
 
             if (other)
                 other.rate += sim.rate
             else
-                reducedSims.push({
+                reducedSims[sim.const + 1] = {
                     const: sim.const,
                     rate: sim.rate
-                })
+                }
         })
 
         return reducedSims.filter(k => k.rate >= 0.006)
@@ -120,9 +138,9 @@ ${createTable(
             const newSims: Sim[] = []
 
             const addOrMerge = (sim: Sim) => {
-                if (sim.rate < 0) return
+                if (sim.rate <= 0) return
 
-                const v = ((+sim.guaranteed) * 10 + (sim.const + 1)) * 100 + sim.pity
+                const v = ((+sim.guaranteed) * (banner.maxConst + 1) + (sim.const + 1)) * 100 + sim.pity
                 const other = newSims[v]
 
                 if (other) {
@@ -134,8 +152,9 @@ ${createTable(
             }
 
             for (const sim of sims) {
+                if (!sim) continue
                 if (sim.rate <= prune) continue // Pruning
-                if (sim.const >= 6) { // Limited to C6
+                if (sim.const >= banner.maxConst) { // Limited to C6
                     addOrMerge(sim)
                     continue
                 }
@@ -144,31 +163,42 @@ ${createTable(
                 const bannerRate = sim.guaranteed ? 1 : banner.banner
 
                 // Failed
-                addOrMerge({
-                    pity: currentPity,
-                    guaranteed: sim.guaranteed,
-                    const: sim.const,
-                    rate: sim.rate * (1 - rate)
-                })
+                if (rate < 1)
+                    addOrMerge({
+                        pity: currentPity,
+                        guaranteed: sim.guaranteed,
+                        const: sim.const,
+                        rate: sim.rate * (1 - rate)
+                    })
 
-                // Got banner
+                // Got wanted banner item
                 addOrMerge({
                     pity: 0,
                     guaranteed: false,
                     const: sim.const + 1,
-                    rate: sim.rate * rate * bannerRate
+                    rate: sim.rate * rate * bannerRate * banner.guaranteed
                 })
 
-                // Failed banner
-                addOrMerge({
-                    pity: 0,
-                    guaranteed: true,
-                    const: sim.const,
-                    rate: sim.rate * rate * (1 - bannerRate)
-                })
+                // Got banner item but not wanted
+                if (banner.guaranteed < 1)
+                    addOrMerge({
+                        pity: 0,
+                        guaranteed: false,
+                        const: sim.const,
+                        rate: sim.rate * rate * bannerRate * (1-banner.guaranteed)
+                    })
+
+                // Failed banner item
+                if (bannerRate < 1)
+                    addOrMerge({
+                        pity: 0,
+                        guaranteed: true,
+                        const: sim.const,
+                        rate: sim.rate * rate * (1 - bannerRate)
+                    })
             }
 
-            sims = newSims.filter(x => x)
+            sims = newSims
         }
         return sims
     }
