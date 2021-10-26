356594afd08dc67511dbcb361fe0ff9cdc43303b
Tibo <Tibowl@users.noreply.github.com>
2021-10-26 12:27:32 +0200

    Add legal disclaimer

--- src/commands/meta/about.ts
+++ src/commands/meta/about.ts
@@ -18,7 +18,9 @@ export default class About extends Command {
     async run(message: Message): Promise<Message | Message[]> {
         const me = (await client.users.fetch("127393188729192448")).tag
         return sendMessage(message, `This is an open-source bot created by @${me}. The source-code is available on GitHub: <https://github.com/Tibowl/HuTao>.
-Data is compiled from a vareity of sources (including but not limited to official forum posts/e-mails/videos/posts elsewhere and Genshin Impact Wiki).
+Data is compiled from a variety of sources (including but not limited to official forum posts/e-mails/videos/posts elsewhere and Genshin Impact Wiki <https://genshin-impact.fandom.com/wiki/Genshin_Impact_Wiki>).
+© All rights reserved by miHoYo. Other properties belong to their respective owners.
+
 You can invite this bot with <https://discord.com/api/oauth2/authorize?client_id=826550363355086918&scope=bot&permissions=319552>.
 
 Feel free to contact me in case there's an error/need support/got a suggestion via <https://discord.gg/BM3Srp8j8G> or via GitHub issues (link above).`)
