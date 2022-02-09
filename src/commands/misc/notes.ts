992949cce21b4cabe772ff45cdc0c1b05c9747fc
Tibowl <Tibowl@users.noreply.github.com>
2022-02-09 11:52:04 +0100

    Merge note commands

--- src/commands/misc/notes.ts
+++ src/commands/misc/notes.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, Message, MessageEmbed } from "discord.js"
+import { CommandInteraction, Guild, Message, MessageEmbed, User } from "discord.js"
 import config from "../../data/config.json"
 import client from "../../main"
 import Command from "../../utils/Command"
@@ -11,35 +11,107 @@ export default class Notes extends Command {
         super({
             name,
             category: "Misc",
-            help: `List of notes.
+            help: `View, create, edit or delete notes.
 
 Notes are either per category (when used in a guild) or per user (when used in DM's).
 
-Requires "Manage Messages" permissions to add, but anyone can see.`,
-            usage: "notes",
-            aliases: ["viewnotes", "no"],
-            options: []
+Requires "Manage Messages" permissions to create/edit/remove, but anyone can see.`,
+            usage: "notes list | notes create <name> | notes edit <id> <name> | notes remove <id>",
+            aliases: ["no", "note"],
+            options: [{
+                name: "list",
+                description: "View notes",
+                type: "SUB_COMMAND",
+            }, {
+                name: "create",
+                description: "Create a new note",
+                type: "SUB_COMMAND",
+                options: [{
+                    name: "name",
+                    description: "Name to show",
+                    type: "STRING",
+                    required: true
+                }]
+            }, {
+                name: "edit",
+                description: "Edits a note by ID",
+                type: "SUB_COMMAND",
+                options: [{
+                    name: "id",
+                    description: "ID to edit",
+                    type: "INTEGER",
+                    required: true
+                }, {
+                    name: "name",
+                    description: "New name",
+                    type: "STRING",
+                    required: true
+                }]
+            }, {
+                name: "remove",
+                description: "Removes a note by ID",
+                type: "SUB_COMMAND",
+                options: [{
+                    name: "id",
+                    description: "ID to remove",
+                    type: "INTEGER",
+                    required: true
+                }]
+            }]
         })
     }
 
     async runInteraction(source: CommandInteraction): Promise<SendMessage | undefined> {
-        return this.run(source)
+        const { options } = source
+        const sub = options.getSubcommand()
+
+        if (sub == "list")
+            return this.runList(source)
+        else if (sub == "create")
+            return this.runCreate(source, options.getString("name", true))
+        else if (sub == "edit")
+            return this.runEdit(source, options.getInteger("id", true), options.getString("name", true))
+        else if (sub == "remove")
+            return this.runRemove(source, options.getInteger("id", true))
+        else
+            return sendMessage(source, `Unknown subcommand ${sub}`)
     }
 
-    async runMessage(source: Message|CommandInteraction): Promise<SendMessage | undefined> {
-        return this.run(source)
+    async runMessage(source: Message|CommandInteraction, args: string[]): Promise<SendMessage | undefined> {
+        const sub = args[0]?.toLowerCase() ?? "list"
+        args.shift()
+
+        if (["list", "l"].includes(sub)) {
+            return this.runList(source)
+        } else if (["create", "add", "a", "c", "new"].includes(sub)) {
+            if (args.length < 1) return this.sendHelp(source)
+
+            return this.runCreate(source, args.join(" "))
+        } else if (["remove", "delete", "d", "r"].includes(sub)) {
+            if (args.length < 1) return this.sendHelp(source)
+
+            const id = parseInt(args[0])
+            if (isNaN(id)) return this.sendHelp(source)
+            return this.runRemove(source, id)
+        } else if (["edit", "e", "change", "update"].includes(sub)) {
+            if (args.length < 1) return this.sendHelp(source)
+
+            const id = parseInt(args[0])
+            if (isNaN(id)) return this.sendHelp(source)
+
+            const name = args.slice(1).join(" ")
+
+            return this.runEdit(source, id, name)
+        } else {
+            return sendMessage(source, `Unknown subcommand \`${sub}\``)
+        }
     }
 
-    async run(source: CommandSource): Promise<SendMessage | undefined> {
+    async runList(source: CommandSource): Promise<SendMessage | undefined> {
         const { notesManager } = client
         const user = getUser(source)
 
-        const guild = source.guild
-        const category = getCategory(source)
-        const name = category?.name ?? guild?.name ?? user.username
-
-        const guildID = guild?.id ?? "user"
-        const categoryID = category?.id ?? guild?.id ?? user.id
+        const { guildID, categoryID, name } = this.getContext(source, user)
 
         const notes = notesManager.getNotes(guildID, categoryID)
 
@@ -50,7 +122,100 @@ Requires "Manage Messages" permissions to add, but anyone can see.`,
         return undefined
     }
 
-    getNotePages(notes: Note[]): string[] {
+    async runCreate(source: CommandSource, subject: string): Promise<SendMessage | undefined> {
+        const { notesManager } = client
+        const user = getUser(source)
+
+        const { guild, guildID, categoryID, name } = this.getContext(source, user)
+
+        const response = this.testPermissions(guild, source)
+        if (response) return response
+
+        const notes = notesManager.getNotes(guildID, categoryID)
+        if (notes.length >= 50) return sendMessage(source, `You can only have up to 50 notes in ${name}, see \`${config.prefix}notes\` for which you have`)
+
+        if (subject.length > 256) return sendMessage(source, "Note name too long")
+
+        let id = 1
+        while (notes.some(r => r.id == id) || notesManager.getNoteById(guildID, categoryID, id))
+            id++
+
+        notesManager.addNote(guildID, categoryID, id, subject, user.id)
+        const reply = sendMessage(source, new MessageEmbed()
+            .setTitle(`Created note #${id} in ${name}`)
+            .setColor(Colors.GREEN)
+            .setDescription(`The note #${id}: \`${subject}\` has been created`)
+        )
+
+        return reply
+    }
+    async runRemove(source: CommandSource, id: number): Promise<SendMessage | undefined> {
+        const { notesManager } = client
+        const user = getUser(source)
+
+        const { guild, guildID, categoryID, name } = this.getContext(source, user)
+
+        const response = this.testPermissions(guild, source)
+        if (response) return response
+
+        const note = notesManager.getNoteById(guildID, categoryID, id)
+        if (note == undefined) return sendMessage(source, `Could not find note #${id} in ${name}`)
+
+        notesManager.deleteNote(guildID, categoryID, id, user.id)
+        const reply = sendMessage(source, new MessageEmbed()
+            .setTitle(`Deleted note #${id} in ${name}`)
+            .setColor(Colors.RED)
+            .setDescription(`The note #${id}: \`${note.subject}\` has been deleted`)
+        )
+
+        return reply
+    }
+
+
+    async runEdit(source: CommandSource, id: number, newSubject: string): Promise<SendMessage | undefined> {
+        const { notesManager } = client
+        const user = getUser(source)
+
+        const { guild, guildID, categoryID, name } = this.getContext(source, user)
+
+        const response = this.testPermissions(guild, source)
+        if (response) return response
+
+        if (newSubject.length > 256) return sendMessage(source, "Note name too long")
+
+        const note = notesManager.getNoteById(guildID, categoryID, id)
+        if (note == undefined) return sendMessage(source, `Could not find note #${id} in ${name}`)
+
+        notesManager.editNote(guildID, categoryID, id, newSubject, user.id)
+        const reply = sendMessage(source, new MessageEmbed()
+            .setTitle(`Edited note #${id} in ${name}`)
+            .setColor(Colors.ORANGE)
+            .setDescription(`Original: \`${note.subject}\`
+New: \`${newSubject}\``)
+        )
+
+        return reply
+    }
+
+    private testPermissions(guild: Guild | null, source: CommandSource) {
+        if (guild)
+            if (source.member == undefined || typeof source.member.permissions == "string")
+                return sendMessage(source, "Unable to check permissions.", undefined, true)
+            else if (!source.member.permissions.has("MANAGE_MESSAGES"))
+                return sendMessage(source, "You do not have have \"Manage Messages\" permissions in here.", undefined, true)
+    }
+
+    private getContext(source: CommandSource, user: User) {
+        const guild = source.guild
+        const category = getCategory(source)
+        const name = category?.name ?? guild?.name ?? user.username
+
+        const guildID = guild?.id ?? "user"
+        const categoryID = category?.id ?? guild?.id ?? user.id
+        return { guild, guildID, categoryID, name }
+    }
+
+    private getNotePages(notes: Note[]): string[] {
         const pages: string[] = []
         let paging = "", c = 0
         for (const note of notes) {
@@ -66,7 +231,7 @@ Requires "Manage Messages" permissions to add, but anyone can see.`,
         return pages
     }
 
-    getNotePage(pages: string[], name: string, relativePage: number, currentPage: number, maxPages: number): MessageEmbed | undefined {
+    private getNotePage(pages: string[], name: string, relativePage: number, currentPage: number, maxPages: number): MessageEmbed | undefined {
         if (relativePage >= pages.length)
             return undefined
 
